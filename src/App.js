dbd0091c775e2e7856f7d23d0e684d1c3c5b3ca8
lokajaya1 <lokajayandala1@gmail.com>
2024-08-16 16:57:42 +0700

    modified ui

--- src/App.js
+++ src/App.js
@@ -1,22 +1,60 @@
-import "./App.css";
+import "./index.css";
+import "@fortawesome/fontawesome-free/css/all.min.css";
+
+const listItems = [
+  { id: 1, title: "eat", done: false },
+  { id: 2, title: "sleep", done: false },
+];
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <Logo />
+      <Form />
+      <CheckList />
+      <Stats />
     </div>
+  );
+}
+
+function Logo() {
+  return <span className="logo">To - Do List</span>;
+}
+
+function Form() {
+  return (
+    <div className="add-form">
+      <h3>Ada yang mau di catat</h3>
+    </div>
+  );
+}
+
+function CheckList() {
+  return (
+    <div className="list">
+      <ul>
+        {listItems.map((item) => (
+          <li key={item.id}>
+            <input type="checkbox" />
+            <span>{item.title}</span>
+            <button>
+              <i
+                class="fa fa-close"
+                style={{ fontSize: "24px", color: "red" }}
+              ></i>
+            </button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+function Stats() {
+  return (
+    <footer className="stats">
+      <span>stats</span>
+    </footer>
   );
 }
 
