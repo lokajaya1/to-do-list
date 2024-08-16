5d8eddd620bf288ea8c439a918c9969734a96e7b
lokajaya1 <lokajayandala1@gmail.com>
2024-08-16 17:49:18 +0700

    modifie appjs

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
+import { useState } from "react";
 import "./index.css";
 import "@fortawesome/fontawesome-free/css/all.min.css";
 
 const listItems = [
   { id: 1, title: "eat", done: false },
-  { id: 2, title: "sleep", done: false },
+  { id: 2, title: "sleep", done: true },
 ];
 
 function App() {
@@ -22,10 +23,29 @@ function Logo() {
 }
 
 function Form() {
+  const [title, setTitle] = useState("");
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    console.log(e);
+  }
+
   return (
-    <div className="add-form">
+    <form className="add-form" onSubmit={handleSubmit}>
       <h3>Ada yang mau di catat</h3>
-    </div>
+      <input
+        type="text"
+        name="title"
+        id=""
+        value={title}
+        onChange={(e) => {
+          setTitle(e.target.value);
+        }}
+      />
+      <button>
+        <i class="fa fa-add" style={{ color: "white" }}></i>
+      </button>
+    </form>
   );
 }
 
@@ -34,19 +54,24 @@ function CheckList() {
     <div className="list">
       <ul>
         {listItems.map((item) => (
-          <li key={item.id}>
-            <input type="checkbox" />
-            <span>{item.title}</span>
-            <button>
-              <i
-                class="fa fa-close"
-                style={{ fontSize: "24px", color: "red" }}
-              ></i>
-            </button>
-          </li>
+          <Item key={item.id} item={item} />
         ))}
       </ul>
     </div>
+  );
+}
+
+function Item({ item }) {
+  return (
+    <li>
+      <input type="checkbox" />
+      <span style={{ textDecoration: item.done ? "line-through" : "" }}>
+        {item.title}
+      </span>
+      <button>
+        <i class="fa fa-close" style={{ fontSize: "24px", color: "red" }}></i>
+      </button>
+    </li>
   );
 }
 
